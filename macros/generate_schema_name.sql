48373af329e6c35da203551631c80c5f76d2453d
carola.giacchetta@theinformationlab.es <carola.giacchetta@theinformationlab.es>
2026-06-16 08:20:21 +0000

    Day 1

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,8 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
+--This tells dbt: if a model has a custom schema defined, use that name exactly. If not (like the example models), use the default dev schema.
